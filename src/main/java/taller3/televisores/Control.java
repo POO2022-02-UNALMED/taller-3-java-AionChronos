bc1ec7254fba212279d02eb525ef2426c2cc8aca
Julián Orrego Martínez <70676228+AionChronos@users.noreply.github.com>
2022-08-29 23:26:46 -0500

    1.0

---
+++ src/main/java/taller3/televisores/Control.java
@@ -0,0 +1,15 @@
+package taller3.televisores;
+
+public class Marca2 {
+	public String nombre;
+	public Marca2(String nombre) {
+		this.nombre= nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre= nombre;
+	}
+	public String getNombre() {
+		return this.nombre;
+	}
+}
